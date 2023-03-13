cb83352b054608a1242fb4ef03a5af45786ea602
Baltrush23 <110633010+Andrius23zim@users.noreply.github.com>
2023-03-13 23:10:41 +0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+
+    const myInput = document.getElementById('myInput');
+    const myDiv = document.getElementById('myDiv');
+    
+    myInput.addEventListener('focus', function() {
+      myDiv.style.display = 'block';
+    });
+    
+    myInput.addEventListener('blur', function() {
+      myDiv.style.display = 'none'; 
+    });
+
+    
+    
+
+    
+    
+    
+
+    
+    
+    
+
+    
+    
+    
+
+    
+    
+    
